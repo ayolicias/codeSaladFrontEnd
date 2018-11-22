a954b251ecb38ca4b41a2a422e56f02c1db77e6f
ayolicias <ziyandandlamafa47@gmail.com>
2018-11-22 17:41:48 +0200

    added files

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+var app = new Vue({
+    el: '#outfit',
+    data: {
+      head: 'hat.jpg',
+      accessories: 'necklace.jpeg',
+      dress: 'dress1.jpg',
+      shoes: 'heels.jpg'
+    }
+  })
